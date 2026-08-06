8a08763d4f7bc3928988b4d8853476113a2c169f
bunns <hasikabombay155@gmail.com>
2026-08-06 13:34:32 +0530

    commit

--- src/com/loopstatments/FactorialForLoop.java
+++ src/com/loopstatments/FactorialForLoop.java
@@ -1,10 +1,17 @@
 package com.loopstatments;
-
+import java.util.Scanner;
 public class FactorialForLoop {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n = sc.nextInt();
+		long fact = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			fact = fact * i;
+}
+	System.out.println("Factorial of " + n + " is: " + fact);
+	sc.close();
 	}
-
 }
--- src/com/loopstatments/testdemoLP.java
+++ src/com/loopstatments/testdemoLP.java
@@ -1,10 +1,29 @@
 package com.loopstatments;
+import java.util.Scanner;
 
 public class testdemoLP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n = sc.nextInt();
 
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+		for (int i = 1; i <= 20; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+		for (int i = 1; i <= 30; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+		for (int i = 1; i <= 40; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+		
+		
+		
+
+		sc.close();
 	}
-
 }
