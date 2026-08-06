f5c53e2ccb9dc3ff6aa1a097fd93ee2afca2c0bf
bunns <hasikabombay155@gmail.com>
2026-08-06 13:13:11 +0530

    commit

---
+++ src/com/loopstatments/FactorialForLoop.java
@@ -0,0 +1,10 @@
+package com.loopstatments;
+
+public class FactorialForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/loopstatments/testdemoLP.java
@@ -0,0 +1,10 @@
+package com.loopstatments;
+
+public class testdemoLP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
