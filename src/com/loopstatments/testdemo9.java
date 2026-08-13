081dece38a069af5ccb640009a9f2dc9fbc95437
bunns <hasikabombay155@gmail.com>
2026-08-13 10:29:09 +0530

    commit

--- src/com/loopstatments/testdemo9.java
+++ src/com/loopstatments/testdemo9.java
@@ -1,10 +1,28 @@
 package com.loopstatments;
+import java.util.Scanner;
+public class testdemo9{
 
-public class testdemo9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int largest = 0;
+        int temp = num;
 
-	}
+        if (temp < 0) {
+            temp = -temp;
+        }
 
+        while (temp != 0) {
+            int digit = temp % 10;   
+            if (digit > largest) {
+                largest = digit;
+            }
+            temp = temp / 10;       
+        }
+
+        System.out.println("Largest Digit = " + largest);
+        sc.close();
+    }
 }
